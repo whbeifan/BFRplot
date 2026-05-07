5bf41056aac09d3f0dee47aae5aaaf5b247ea651
whbeifan <whbeifan@foxmail.com>
2026-05-07 11:51:32 +0800

    Create run_PLS-SEM.sh

---
+++ PLS-SEM/run_PLS-SEM.sh
@@ -0,0 +1,2 @@
+/Work/pipeline/software/meta/R/v4.2.2/bin/Rscript plot_PLS-SEM.R data_sem.tsv --tran --variable_list  sem_group.list  -p out >out.sem.log
+#/Work/pipeline/software/meta/R/v4.2.2/bin/Rscript /Work/pipeline/BYMNGS/v2.3.8/scripts/plot_PLS-SEM.R data_sem.tsv --tran --variable_list  sem_group.list  --latent_vars latent_vars.list -p out >out.sem.log
